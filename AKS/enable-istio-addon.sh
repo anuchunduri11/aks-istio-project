23f834e1e7a3a0f4f4fb3be785b62d0226e10bf2
Annapurna Chunduri <anuchunduri11@gmail.com>
2023-10-12 10:53:11 +0200

    New folder structure

--- AKS/enable-istio-addon.sh
+++ AKS/enable-istio-addon.sh
@@ -2,7 +2,6 @@
 
 export CLUSTER=anuchunduridevopsaks
 export RESOURCE_GROUP=anuchunduridevops-rg
-export LOCATION=swedencentral
 
 # Install aks-preview extension and udpate to latest version (istio is a aks preview feature)
 az extension add --name aks-preview
@@ -12,3 +11,12 @@ az extension update --name aks-preview
 az feature register --namespace "Microsoft.ContainerService" --name "AzureServiceMeshPreview"
 az feature show --namespace "Microsoft.ContainerService" --name "AzureServiceMeshPreview"
 az provider register -n Microsoft.ContainerService
+
+#If you are to instal Istio add-io for an existing cluster after its creation
+az aks mesh enable --resource-group ${RESOURCE_GROUP} --name ${CLUSTER}
+
+#Verify istio successful installation on AKS cluster
+az aks show --resource-group ${RESOURCE_GROUP} --name ${CLUSTER}  --query 'serviceMeshProfile.mode'
+
+#Get AKS credentials
+az aks get-credentials --name ${CLUSTER} --resource-group ${RESOURCE_GROUP}
